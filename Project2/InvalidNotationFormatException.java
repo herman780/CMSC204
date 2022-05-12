00bc3a5324d89bb7adb7bbe1cbdc489b2e672bb4
Herman Mann <70986277+herman780@users.noreply.github.com>
2022-05-12 19:57:05 -0400

    Create InvalidNotationFormatException.java

---
+++ Project2/InvalidNotationFormatException.java
@@ -0,0 +1,11 @@
+/**
+ * InvalidNotationFormatException class - exception is thrown and occurs when a Notation format is incorrect. 
+ * @author Herman Mann
+ */
+public class InvalidNotationFormatException extends Exception {
+	
+	public InvalidNotationFormatException()
+	{
+		super("The Notation Format is invalid/incorrect.");
+	}
+}
